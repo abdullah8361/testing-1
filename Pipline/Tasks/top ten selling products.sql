ca5571d6e9adff1275c7cdb20453b8b511c334e8
abdullah8361 <ak242536366@gmail.com>
2024-11-15 18:06:25 +0500

    Hello

--- Pipline/Tasks/top ten selling products.sql
+++ Pipline/Tasks/top ten selling products.sql
@@ -1,32 +1,32 @@
-WITH SalesSummary AS (
+WITH SalesData AS (
     SELECT 
         p.product_id,
         p.product_name,
         SUM(s.quantity_sold) AS total_quantity_sold
     FROM 
-        Sales s
+        sales s
     JOIN 
-        Products p ON s.product_id = p.product_id
+        products p ON s.product_id = p.product_id
     WHERE 
-        s.sale_date >= DATE_TRUNC('month', CURRENT_DATE - INTERVAL '1 month') 
+        s.sale_date >= DATE_TRUNC('month', CURRENT_DATE - INTERVAL '1 month')
         AND s.sale_date < DATE_TRUNC('month', CURRENT_DATE)
     GROUP BY 
         p.product_id, p.product_name
 ),
-TopFiveProducts AS (
+RankedProducts AS (
     SELECT 
         product_id,
         product_name,
         total_quantity_sold,
-        ROW_NUMBER() OVER (ORDER BY total_quantity_sold DESC) AS row_num
+        ROW_NUMBER() OVER (ORDER BY total_quantity_sold DESC) AS rank
     FROM 
-        SalesSummary
+        SalesData
 )
 SELECT 
     product_id,
     product_name,
     total_quantity_sold
 FROM 
-    TopFiveProducts
+    RankedProducts
 WHERE 
-    row_num <= 5;
+    rank <= 5;
